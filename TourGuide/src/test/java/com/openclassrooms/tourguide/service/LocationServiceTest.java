3f363c9125b4755f97be8dee64c4be40318b2c29
jonathan-robin <jonathan@levelup-analytics.com>
2025-02-16 16:50:26 +0400

    fix ci

--- TourGuide/src/test/java/com/openclassrooms/tourguide/service/LocationServiceTest.java
+++ TourGuide/src/test/java/com/openclassrooms/tourguide/service/LocationServiceTest.java
@@ -36,6 +36,9 @@ public class LocationServiceTest {
     @Autowired
     private GpsUtil gpsUtils;
     
+    @Autowired
+    private UtilsService utilsService;
+    
     @MockBean
     private LocationService locationService;
 
